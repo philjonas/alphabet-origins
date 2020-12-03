2b1c4d0fb3eb9d3c731af5e83f8d9e8fa514b13c
philjonas <philjonascode@gmail.com>
2020-12-03 04:17:39 +0000

    styling

--- src/components/HieroglyphicsHeader/index.tsx
+++ src/components/HieroglyphicsHeader/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { LogogramType, StateType } from '../../store/types';
+import './style.css';
 
 const mapStateToProps = (state: StateType) => {
     const { logograms } = state
@@ -11,7 +12,11 @@ const mapStateToProps = (state: StateType) => {
 export const HieroglyphicsTemplate = ({ logograms }: { logograms: LogogramType[] }) => {
     const logogramComponents = logograms.map(logo => {
         const linkTo = '/' + logo.id?.toString()
-        return <Link to={linkTo}>{logo.hieroglyph}</Link>
+        return (
+            <Link to={linkTo}>
+                <span className="hieroglyph-btn">{logo.hieroglyph}</span>
+            </Link>
+        )
     })
 
     return (
