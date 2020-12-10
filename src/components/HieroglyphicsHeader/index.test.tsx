8d76585e57cbe4a0faa2cafe874e8943c024ec59
philjonas <philjonascode@gmail.com>
2020-12-10 20:25:32 +0000

    mapstatetoprops tested

--- src/components/HieroglyphicsHeader/index.test.tsx
+++ src/components/HieroglyphicsHeader/index.test.tsx
@@ -2,12 +2,41 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
+import { HieroglyphicsTemplate } from '.';
+import configureMockStore from 'redux-mock-store';
+import { LogogramType, StateType } from '../../store/types';
+import { connect, Provider } from "react-redux";
 
 Enzyme.configure({ adapter: new Adapter() })
-import {HieroglyphicsHeader} from '.';
+const mockStore = configureMockStore();
+
+const mapStateToProps = (state: StateType) => {
+  const { logograms } = state
+  return { logograms }
+}
 
 
-test('matches snapshot', () => {
-  const wrapper = shallow(<HieroglyphicsHeader />)
-  expect(toJson(wrapper)).toMatchSnapshot();
+describe('HieroglyphicsHeader', () => {
+
+  let wrapper: any;
+  let store;
+
+  beforeEach(() => {
+    const initialState: StateType = {
+      logograms: [],
+      id: -1
+    };
+    store = mockStore(initialState)
+    const HieroglyphicsHeader = connect(mapStateToProps)(HieroglyphicsTemplate)
+    wrapper = shallow(
+      <Provider store={store}>
+        <HieroglyphicsHeader />
+      </Provider>
+    );
+  });
+
+  test('matches snapshot', () => {
+    // const wrapper = shallow(<HieroglyphicsHeader />)
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
 })
