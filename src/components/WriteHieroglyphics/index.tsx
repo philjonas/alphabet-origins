ef19388fa583e30730ee046fe0ee8287e4ab8054
philjonas <philjonascode@gmail.com>
2020-12-12 12:14:38 +0000

    getting there

--- src/components/WriteHieroglyphics/index.tsx
+++ src/components/WriteHieroglyphics/index.tsx
@@ -1,32 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { StateType } from '../../store/types'
+import { connect } from "react-redux";
 import './style.css';
 
-const people : string[] = [
-    "Siri",
-    "Alexa",
-    "Google",
-    "Facebook",
-    "Twitter",
-    "Linkedin",
-    "Sinkedin",
-  ];
+const mapStateToProps = (state: StateType) => {
+    const { letterMappings } = state
+    // letterMappings: Record<string, number>
+    return { letterMappings }
+}
 
-export const WriteHieroglyphics: React.FunctionComponent = () => {
+export const WriteHieroglyphicsTemplate = ({ letterMappings }: { letterMappings: Record<string, number> }) => {
     const [searchTerm, setSearchTerm] = useState("");
     const s: string[] = []
     const [searchResults, setSearchResults] = useState(s);
-    const handleChange = (event:React.InputHTMLAttributes<HTMLInputElement>) => {
+    const handleChange = (event: React.InputHTMLAttributes<HTMLInputElement>) => {
         const target = (event as InputEvent).target
-        if(!target) return
+        if (!target) return
         const value = (target as any).value
-        console.log(value);
         setSearchTerm(value);
     };
     useEffect(() => {
-        const results: string[] = people.filter((person) =>
-            person.toLowerCase().includes(searchTerm)
-        );
-        setSearchResults(results);
+        // const results: string[] = letterMappings.filter((person) =>
+        //     person.toLowerCase().includes(searchTerm)
+        // );
+        // setSearchResults(results);
+        //console.log(key, value)
+        const results : number[] = []
+        Object.entries(letterMappings).forEach(
+            ([key, value]) => {
+                if (searchTerm.includes(key)) results.push(value)
+            }
+        )
+        console.log(results)
     }, [searchTerm]);
 
     return (
@@ -45,3 +50,5 @@ export const WriteHieroglyphics: React.FunctionComponent = () => {
         </div>
     );
 };
+
+export const WriteHieroglyphics = connect(mapStateToProps)(WriteHieroglyphicsTemplate);
