583ff1c31eeb3a004153eee58e8cc21def8a8f2a
philjonas <philjonascode@gmail.com>
2020-12-12 11:46:02 +0000

    demo works again

--- src/components/WriteHieroglyphics/index.tsx
+++ src/components/WriteHieroglyphics/index.tsx
@@ -1,7 +1,47 @@
-import * as React from 'react';
+import React, { useEffect, useState } from "react";
 import './style.css';
 
+const people : string[] = [
+    "Siri",
+    "Alexa",
+    "Google",
+    "Facebook",
+    "Twitter",
+    "Linkedin",
+    "Sinkedin",
+  ];
 
 export const WriteHieroglyphics: React.FunctionComponent = () => {
-    return <div>write</div>
-}
+    const [searchTerm, setSearchTerm] = useState("");
+    const s: string[] = []
+    const [searchResults, setSearchResults] = useState(s);
+    const handleChange = (event:React.InputHTMLAttributes<HTMLInputElement>) => {
+        const target = (event as InputEvent).target
+        if(!target) return
+        const value = (target as any).value
+        console.log(value);
+        setSearchTerm(value);
+    };
+    useEffect(() => {
+        const results: string[] = people.filter((person) =>
+            person.toLowerCase().includes(searchTerm)
+        );
+        setSearchResults(results);
+    }, [searchTerm]);
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search"
+                value={searchTerm}
+                onChange={handleChange}
+            />
+            <ul>
+                {searchResults.map((item) => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
