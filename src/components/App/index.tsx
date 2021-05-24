3260972f27fdf76052b6dbf9a5e4fe9a40bc764b
wholetonegames <philjonascode@gmail.com>
2021-05-24 15:13:46 +0100

    leveraging github pages

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   Link,
@@ -13,28 +12,26 @@ import './style.css';
 export const App = () => {
   return (
     <div className="App">
-      <Router>
-        <h1 className="page-title">The Hieroglyphic Origins of the Greek and Latin Alphabets</h1>
-        <div className="link-container">
-          <Link to="/">Home</Link>
-          <Link to="/write">Write in Hieroglyphics and Phoenician</Link>
-        </div>
-        <Switch>
-          <Route exact path="/write">
-            <WriteHieroglyphics />
-          </Route>
-          <Route exact path="/">
-            <EtymologyPage />
-          </Route>
-          <Route exact path="/hieroglyphics/:id">
-            <EtymologyPage />
-          </Route>
-          <Route path="*">
-            <EtymologyPage />
-          </Route>
-        </Switch>
-        <p className="etymology-info">Information taken from <a href="https://en.wikipedia.org/wiki/Phoenician_alphabet" target="_blank" rel="noreferrer"> Wikipedia's Phoenician Alphabet</a> page</p>
-      </Router>
+      <h1 className="page-title">The Hieroglyphic Origins of the Greek and Latin Alphabets</h1>
+      <div className="link-container">
+        <Link to="/">Home</Link>
+        <Link to="/write">Write in Hieroglyphics and Phoenician</Link>
+      </div>
+      <Switch>
+        <Route exact path="/write">
+          <WriteHieroglyphics />
+        </Route>
+        <Route exact path="/">
+          <EtymologyPage />
+        </Route>
+        <Route exact path="/hieroglyphics/:id">
+          <EtymologyPage />
+        </Route>
+        <Route path="*">
+          <EtymologyPage />
+        </Route>
+      </Switch>
+      <p className="etymology-info">Information taken from <a href="https://en.wikipedia.org/wiki/Phoenician_alphabet" target="_blank" rel="noreferrer"> Wikipedia's Phoenician Alphabet</a> page</p>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from './store/index';
 import './index.css';
 import { App } from './components/App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 // TODO: remove this, just exposing the store to the console for a bit ;)//////
 (window as any).store = store;
@@ -12,7 +13,9 @@ import { App } from './components/App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router basename={process.env.PUBLIC_URL}>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
