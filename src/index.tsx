886a7976ddff06f790d4ce62d31ff02a08a420f3
wholetonegames <philjonascode@gmail.com>
2021-07-04 15:43:51 +0100

    publishing new version

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import store from './store/index';
 import './index.css';
-
 import { HashRouter } from "react-router-dom";
 import { App } from './containers/App';
 
